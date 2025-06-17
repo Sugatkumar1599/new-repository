b6c17c039cfcfd1726d6f177c71c44462b37ee58
Sugatkumar1599 <1sugatkumar@gmail.com>
2025-06-17 13:28:13 +0530

    this ismit

--- OneDrive/Desktop/sugat/my-app/mypandas.py
+++ OneDrive/Desktop/sugat/my-app/mypandas.py
@@ -14,37 +14,9 @@ def predict_salary(years_of_experience):
     df=pd.DataFrame(raw_data)
 
     X= np.array(df['years_worked']).reshape(-1,1)
-    y= np.array(df['salary']).reshape(-1,1)
+    y= np.array(df['salary'])
+
+    print(X)
+    print(y)
     
-  
-#    #split the training data and testist data
-#     train_X,test_X,train_y,test_y=train_test_split(X ,y, random_state=0 ,test_size=.20) 
-#     # print('train_X',train_X.tolist())
-#     # print('train_y',train_X.tolist())
-#     # print('test_X',test_X.tolist())
-#     # print('test_y',test_y.tolist())
-
-
-#     #Intialise the data 
-#     model= LinearRegression()
-#     model.fit(train_X,train_y)
-
-#     #Make the prediction
-#     y_prediction = model.predict([[years_of_experience]])
-#     print('Prediction:',y_prediction)
-#     #used to check how accurate the model is'
-#     y_test_prediction = model.predict(test_X)
-#     y_line = model.predict(X)
-#     #Extra_info
-#     print('Slope :',model.coef_)
-#     print('Intercept :',model.intercept_)
-#     print('MAE :',mean_absolute_error(test_y,y_test_prediction))
-#     print('r2 :',r2_score(test_y,y_test_prediction))
-
-#     plt.scatter(X, y, s=12)
-#     plt.xlabel('Years (Exp)')
-#     plt.ylabel('salary')
-#     plt.plot(X,y_line, color='r')
-#     plt.show()
-
-predict_salary(14)
+predict_salary(10)
