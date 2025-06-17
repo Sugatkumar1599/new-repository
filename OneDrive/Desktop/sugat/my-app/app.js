234982fdf753ac66309669b79572ded3ab1a9adc
Sugatkumar1599 <1sugatkumar@gmail.com>
2025-06-17 10:30:47 +0530

    this is third repository.

---
+++ OneDrive/Desktop/sugat/my-app/app.js
@@ -0,0 +1,4 @@
+function sayHello() {
+    alert("Hello from your web app!");
+  }
+  
---
+++ OneDrive/Desktop/sugat/my-app/my-app/src/components/Home.js
@@ -0,0 +1,26 @@
+const Home = () => (
+    <div>
+      <div>
+        <h1>Harry Parker</h1>
+        <button>Search icon</button>
+      </div>
+      <div>
+        <div>
+          <h2>New Place</h2>
+          <a>See more</a>
+          <div>
+            <img src="#" alt="#" />
+          </div>
+        </div>
+        <div>
+          <h2>Popular Hotel</h2>
+           <a>See more</a>
+          <div>
+            <img src="#" alt="#" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+  
+  export default Home;
